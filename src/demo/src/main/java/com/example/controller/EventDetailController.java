962daba3fed8a4fe0cc466a662253cc2fd2bcb18
arimura-t <arimura-t@kkhts.com>
2019-01-07 22:59:59 +0900

    有村　CSVの出力

--- src/demo/src/main/java/com/example/controller/EventDetailController.java
+++ src/demo/src/main/java/com/example/controller/EventDetailController.java
@@ -1,11 +1,16 @@
 package com.example.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.dto.EventDetailDto;
@@ -49,14 +54,27 @@ public class EventDetailController {
 		// 各メンバーの出欠、コメントを取得
 		boolean isRegist = false;
 		List<EventDetailDto> eventDetailList = new ArrayList<>();
+		//イベント日付取得
+		String csvText = "";
+
+		for (EventDate eventDate : eventDateList) {
+				csvText = csvText + ","+eventDate.getStartDate();
+		}
+		csvText = csvText + ",致知読書感想文,コメント";
+		csvText = csvText + "\n";
 		for (Member member : memberList) {
+			// メンバー名
+			csvText = csvText + member.getName();
+
 			// 出欠の取得
 			List<String> attendanceList = new ArrayList<>();
 			for (EventDate eventDate : eventDateList) {
 				EventAttendance eventAttendance = eventAttendanceService.findByPK(member.getId(), eventDate.getId());
 				if (eventAttendance != null) {
 					attendanceList.add(eventAttendance.getAttendance());
+					csvText = csvText + ","+eventAttendance.getAttendance();
 				} else {
+					csvText = csvText + ",";
 					break;
 				}
 			}
@@ -81,14 +99,29 @@ public class EventDetailController {
 			if (eventComment != null) {
 				dto.setComment(eventComment.getComment());
 				dto.setBookAttendance(eventComment.getBookattendance());
+				if(eventComment.getBookattendance() == null){
+					csvText = csvText + ",";
+				}else{
+					csvText = csvText + ","+eventComment.getBookattendance();
+				}
+
+				if(eventComment.getComment() == null){
+					csvText = csvText + ",";
+				}else{
+					csvText = csvText + ","+eventComment.getComment();
+				}
+
+
 			}
 
 			eventDetailList.add(dto);
+			csvText = csvText + "\n";
 		}
 
 		model.addAttribute("eventDateList", eventDateList);
 		model.addAttribute("eventDetailList", eventDetailList);
 		model.addAttribute("isRegist", isRegist);
+		model.addAttribute("csvText", csvText);
 
 		model.addAttribute("sessionModel", sessionModel);
 
@@ -99,4 +132,30 @@ public class EventDetailController {
 	public String transitionAttendance(Model model, EventDetailModel eventDetailModel) {
 		return "forward:/attendance?eventId=" + eventDetailModel.getEventid() + "&memberId=" + sessionModel.getId();
 	}
+
+	@RequestMapping(value = "/csvOutput")
+	public String csvOutput(HttpServletResponse response, String csvText)
+	{
+
+
+		//文字コードと出力するCSVファイル名を設定
+        response.setContentType(MimeTypeUtils.APPLICATION_OCTET_STREAM_VALUE + ";charset=sjis");
+        response.setHeader("Content-Disposition", "attachment; filename=\"ouput.csv\"");
+
+
+		try (PrintWriter pw = response.getWriter()) {
+
+
+                //CSVファイルに書き込み
+                pw.print(csvText);
+
+		} catch (IOException e) {
+            e.printStackTrace();
+        }
+
+		//model.addAttribute("sessionModel", sessionModel);
+
+		return "eventDetail";
+	}
+
 }
